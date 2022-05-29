3fecb3a10aca122041179505cb63a6d02db73513
not-sync <100706802+not-sync@users.noreply.github.com>
2022-05-29 15:49:02 -0400

    feat: update design

--- functions/thumbnail.js
+++ functions/thumbnail.js
@@ -1,10 +1,42 @@
 const chromium = require("chrome-aws-lambda");
-const puppeteer = require("puppeteer-core");
+const fs = require("fs");
 
-const browser = await puppeteer.launch({
-  args: chromium.args,
-  executablePath: await chromium.executablePath,
-  headless: true,
-});
+exports.handler = async function (event, context) {
+  const { title, tagColor, tagTitle, imageURL } = JSON.parse(event.body);
+  const executablePath = await chromium.executablePath,
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      headless: chromium.headless,
+    });
 
-await browser.close();
+  const page = await browser.newPage();
+  const html = fs.readFileSync("index.html", "utf-8");
+
+  await page.setContent(html);
+  // "https://n-magazine.com/wp-content/uploads/2021/04/spotify-icon-green-logo-8.png"
+
+  await page.evaluate(
+    (title, tagColor, tagTitle, imageURL) => {
+      document.querySelector("#title").innerHTML = title;
+      document.querySelector(".tag").innerHTML = tagTitle;
+      document.querySelector(".tag").style.backgroundColor = tagColor;
+      document.querySelector("#image").setAttribute("src", imageURL);
+    },
+    title,
+    tagColor,
+    tagTitle,
+    imageURL
+  );
+
+  const screenshot = await page.screenshot({ encoding: "binary" });
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      buffer: screenshot,
+    }),
+  };
+};
---
+++ public/script.js
@@ -0,0 +1,54 @@
+function bufferToImageUrl(buffer) {
+  // See https://gist.github.com/candycode/f18ae1767b2b0aba568e
+
+  var arrayBufferView = new Uint8Array(buffer);
+  var blob = new Blob([arrayBufferView], { type: "image/jpeg" });
+  var urlCreator = window.URL || window.webkitURL;
+  var imageUrl = urlCreator.createObjectURL(blob);
+
+  return imageUrl;
+}
+
+document
+  .querySelector('button[type="submit"]')
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let { title, tagColor, tagTitle, imageURL } =
+      document.querySelector("#thumbmeta").elements;
+
+    // if (!pageToScreenshot)
+    //   return (document.getElementById("result").textContent =
+    //     "Please enter a page URL");
+
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify({
+        title: title.value,
+        tagColor: tagColor.value,
+        tagTitle: tagTitle.value,
+        imageURL: imageURL.value,
+      }),
+    };
+
+    document.getElementById("result").textContent = "Please wait...";
+
+    fetch("/.netlify/functions/thumbnail", options)
+      .then((res) => res.json())
+      .then((res) => {
+        if (!res.buffer)
+          return (document.getElementById("result").textContent =
+            "Error capturing screenshot");
+
+        const img = document.createElement("img");
+        img.src = bufferToImageUrl(res.buffer.data);
+        document.getElementById("result").innerHTML = img.outerHTML;
+      })
+      .catch((err) => {
+        console.log(err);
+        document.getElementById(
+          "result"
+        ).textContent = `Error: ${err.toString()}`;
+      });
+  });
