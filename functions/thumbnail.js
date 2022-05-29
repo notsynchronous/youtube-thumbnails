926e3643ccc0bce46f09845454523d3851bbe47a
not-sync <100706802+not-sync@users.noreply.github.com>
2022-05-29 17:10:26 -0400

    fix: update browser object

--- functions/thumbnail.js
+++ functions/thumbnail.js
@@ -3,36 +3,42 @@ const fs = require("fs");
 
 exports.handler = async function (event, context) {
   const { title, tagColor, tagTitle, imageURL } = JSON.parse(event.body);
+  let screenshot = null;
+  let browser = null;
 
-  const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    headless: chromium.headless,
-    executablePath: await chromium.executablePath,
-  });
+  try {
+    await chromium.executablePath,
+      (browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless,
+      }));
 
-  const page = await browser.newPage();
-  const html = fs.readFileSync("index.html", "utf-8");
+    const page = await browser.newPage();
+    const html = fs.readFileSync("./index.html", "utf-8");
 
-  await page.setContent(html);
-  // "https://n-magazine.com/wp-content/uploads/2021/04/spotify-icon-green-logo-8.png"
+    await page.setContent(html);
+    // "https://n-magazine.com/wp-content/uploads/2021/04/spotify-icon-green-logo-8.png"
 
-  await page.evaluate(
-    (title, tagColor, tagTitle, imageURL) => {
-      document.querySelector("#title").innerHTML = title;
-      document.querySelector(".tag").innerHTML = tagTitle;
-      document.querySelector(".tag").style.backgroundColor = tagColor;
-      document.querySelector("#image").setAttribute("src", imageURL);
-    },
-    title,
-    tagColor,
-    tagTitle,
-    imageURL
-  );
+    await page.evaluate(
+      (title, tagColor, tagTitle, imageURL) => {
+        document.querySelector("#title").innerHTML = title;
+        document.querySelector(".tag").innerHTML = tagTitle;
+        document.querySelector(".tag").style.backgroundColor = tagColor;
+        document.querySelector("#image").setAttribute("src", imageURL);
+      },
+      title,
+      tagColor,
+      tagTitle,
+      imageURL
+    );
 
-  const screenshot = await page.screenshot({ encoding: "binary" });
-
-  await browser.close();
+    screenshot = await page.screenshot({ encoding: "binary" });
+  } catch (err) {
+    return err;
+  } finally {
+    await browser.close();
+  }
 
   return {
     statusCode: 200,
